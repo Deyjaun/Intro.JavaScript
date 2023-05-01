6ae8ab6314ec22ad98e16bb38b3240429cb7974f
Deyjaun Lawrence <Dlawrence@brooklynsteamcenter.org>
2023-05-01 13:42:36 -0400

    initial commit

---
+++ stopwatch.js
@@ -0,0 +1,14 @@
+function start () {
+alert ("you clicked the start button")
+
+}
+
+function stop () {
+alert ("you clicked the stop button")
+
+}
+
+function reset () {
+alert ("you clicked the reset button")
+
+}

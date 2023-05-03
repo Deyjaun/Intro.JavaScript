7670d9799d0a71665e93139793dff72fe36fbe04
Deyjaun Lawrence <Dlawrence@brooklynsteamcenter.org>
2023-05-03 14:53:49 -0400

    lap logged

--- stopwatch.js
+++ stopwatch.js
@@ -6,12 +6,18 @@ let minutes = 0
 let digits = document.getElementById('digits')
 
 let startButton = document.getElementById('start-btn')
+let lapButton = document.getElementById("lbut")
+
+let laplist = document.getElementById('Lap')
+
+lapButton.disabled = true
 
 let interval;
 
 function start() { 
 
     startButton.disabled = true
+    lapButton.disabled = false
     
     
     interval = setInterval (function() {
@@ -33,23 +39,33 @@ function start() {
 
 function stop () {
     clearInterval(interval)
-    startButton.disabled = false
+    lapButton.disabled = true
+    milliseconds = 0
+    seconds = 0
+    minutes = 0
+    digits.innerHTML = "00:00:00"
+    laplist.innerHTML = ""
+
 
 }
 
 function reset() {
+    lapButton.disabled = true
+    startButton.disabled = false
     stop()
     milliseconds = 0
     seconds = 0
     minutes = 0
     digits.innerHTML = "00:00:00"
+    laps.innerHTML = ""
 }
 
 
 function lap()  {
-    const node = document.createElement("li");
-    const textnode = document.createTextNode("Water");
-    node.appendChild(textnode);
-    document.getElementById("myList").appendChild(node);
+     let newListItem = document.createElement("li")
+    newListItem.textContent =  digits.innerHTML = minutes + ':' + String(seconds).padStart(2, '0') + ':' + milliseconds/10
+    laplist.appendChild(newListItem)
+
+
 
 }
